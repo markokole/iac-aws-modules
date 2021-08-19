ed32dd3a3962ece93cf92c55580e2685d8034cc9
Marko Kljajic <marko_kole@yahoo.com>
2021-08-19 13:04:28 +0200

    removed old code

--- terraform/test-ec2/provision.tf
+++ terraform/test-ec2/provision.tf
@@ -1,20 +1,35 @@
 module "vpc" {
     source            = "../vpc"
     project_name      = "Test EC2"
-
 }
 
 module "ec2" {
     source                      = "../ec2"
     project_name                = module.vpc.project_name
-    instance_type               = "t3.medium"
-    ami                         = "ami-0baa9e2e64f3c00db"  # Red Hat Enterprise Linux 8 (HVM), SSD Volume Type
-    security_groups             = [module.vpc.security_group]
-    subnet                      = module.vpc.subnet_public
-    availability_zone           = module.vpc.availability_zone_public
-    key_name                    = "markokey"
-    associate_public_ip_address = true
-    ec2_tag_name                = "${module.vpc.project_name} - public ec2"
+
+    ec2_data = {
+        public  = { instance_type               = "t3.medium"
+                    ami                         = "ami-0baa9e2e64f3c00db",
+                    subnet_id                   = module.vpc.subnet_public,
+                    availability_zone           = module.vpc.availability_zone_public,
+                    security_groups             = [module.vpc.security_group],
+                    key_name                    = "markokey",
+                    associate_public_ip_address = true},
+        private_1 = { instance_type               = "t3.medium"
+                    ami                         = "ami-0baa9e2e64f3c00db",
+                    subnet_id                   = module.vpc.subnet_private,
+                    availability_zone           = module.vpc.availability_zone_private,
+                    security_groups             = [module.vpc.security_group],
+                    key_name                    = "markokey",
+                    associate_public_ip_address = false},
+        private_2 = { instance_type               = "t3.medium"
+                    ami                         = "ami-0baa9e2e64f3c00db",
+                    subnet_id                   = module.vpc.subnet_private,
+                    availability_zone           = module.vpc.availability_zone_private,
+                    security_groups             = [module.vpc.security_group],
+                    key_name                    = "markokey",
+                    associate_public_ip_address = false}
+    }
 }
 
 output "module_ec2_outputs" {
