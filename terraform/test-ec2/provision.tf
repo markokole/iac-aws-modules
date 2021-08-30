5753f73f53ad4240303ddfb9a53990b0a26ceb22
Marko Kljajic <marko_kole@yahoo.com>
2021-08-30 11:25:36 +0200

    vpc outputs became lists

--- terraform/test-ec2/provision.tf
+++ terraform/test-ec2/provision.tf
@@ -17,15 +17,15 @@ module "ec2" {
                     associate_public_ip_address = true},
         private_1 = { instance_type               = "t3.medium"
                     ami                         = "ami-0baa9e2e64f3c00db",
-                    subnet_id                   = module.vpc.subnet_private,
-                    availability_zone           = module.vpc.availability_zone_private,
+                    subnet_id                   = module.vpc.subnet_private[0],
+                    availability_zone           = module.vpc.availability_zone_private[0],
                     security_groups             = [module.vpc.security_group],
                     key_name                    = "markokey",
                     associate_public_ip_address = false},
         private_2 = { instance_type               = "t3.medium"
                     ami                         = "ami-0baa9e2e64f3c00db",
-                    subnet_id                   = module.vpc.subnet_private,
-                    availability_zone           = module.vpc.availability_zone_private,
+                    subnet_id                   = module.vpc.subnet_private[1],
+                    availability_zone           = module.vpc.availability_zone_private[1],
                     security_groups             = [module.vpc.security_group],
                     key_name                    = "markokey",
                     associate_public_ip_address = false}
