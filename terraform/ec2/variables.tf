ca56996cb6f7fc9456842cde4c979ed844bbf63c
Marko Kljajic <marko_kole@yahoo.com>
2021-09-28 11:13:56 +0200

    added add security group rule for ec2

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -6,3 +6,20 @@ variable ec2_data {
   description = "EC2 configuration"
   type        = map
 }
+
+variable security_group_to_add_rule {
+  description = "Security Group which gets a new rule"
+  type        = string
+}
+
+variable security_group_rules {
+    description = "Security group rules added for ECS containers"
+    type = map
+    default = {
+        dummy = {
+            port = 80
+            cidr_blocks = ["127.0.0.0/32"]
+            description = "Dummy port"
+        }
+    }
+}
